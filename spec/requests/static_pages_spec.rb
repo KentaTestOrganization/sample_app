1e5ed70220b38764f18189dbdf294224acbc56b3
Kenta Katsumata <arith.katsumata@gmail.com>
2015-04-27 11:29:59 +0900

    Make a basic User model (including secure passwords)

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,30 +1,6 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-  # describe "GET /static_pages" do
-  #   it "works! (now write some real specs)" do
-  #     # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-  #     get static_pages_index_path
-  #     response.status.should be(200)
-  #   end
-  # end
-
-  # let(:base_title) { "Ruby on Rails Tutorial Sample App" }
-
-  # describe "Home page" do
-
-  #   before { visit root_path }
-
-  #   it "should have the content 'Sample App'" do
-  #     expect(page).to have_content('Sample App')
-  #   end
-  #   it "should have the right title" do
-  #     expect(page).to have_title("#{base_title}")
-  #   end
-  #   it "should not have a custom page title" do
-  #     expect(page).not_to have_title('| Home')
-  #   end
-  # end
 
   subject { page }
 
