e800ee0d9ad6440751c303456c80d8e15f6982d6
Kenta Katsumata <arith.katsumata@gmail.com>
2015-04-28 15:06:31 +0900

    Add user microposts

---
+++ app/controllers/microposts_controller.rb
@@ -0,0 +1,41 @@
+class MicropostsController < ApplicationController
+  before_action :signed_in_user, only: [:create, :destroy]
+  before_action :correct_user,   only: :destroy
+
+  def create
+    @micropost = current_user.microposts.build(micropost_params)
+    if @micropost.save
+      flash[:success] = "Micropost created!"
+      redirect_to root_url
+    else
+      @feed_items = []
+      render 'static_pages/home'
+    end
+  end
+
+  def destroy
+    @micropost.destroy
+    redirect_to root_url
+  end
+
+  private
+
+    def micropost_params
+      params.require(:micropost).permit(:content)
+    end
+
+    def correct_user
+      @micropost = current_user.microposts.find_by(id: params[:id])
+      redirect_to root_url if @micropost.nil?
+    end
+    # 下記の方式もオーケー
+    # def correct_user
+    #   @micropost = current_user.microposts.find(params[:id])
+    # rescue
+    #   redirect_to root_url
+    # end
+    # def correct_user
+    #   @micropost = Micropost.find_by(id: params[:id])
+    #   redirect_to root_url unless current_user?(@micropost.user)
+    # end
+end
